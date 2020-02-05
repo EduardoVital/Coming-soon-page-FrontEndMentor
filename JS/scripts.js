cda7d22fca0b6eeb29d5c1abb2b1e5db34ccfe49
Eduardo Vital <eduardovitals@gmail.com>
2020-02-04 22:17:43 -0300

    update javascript

--- JS/scripts.js
+++ JS/scripts.js
@@ -9,19 +9,16 @@
 //     }
 // }
 
-// const validaForm = document.querySelector('form').addEventListener('submit', function(event){
-//     event.preventDefault()
-//     return validacaoEmail()
-// })
-
 const validaForm = document.querySelector('form').addEventListener('submit', function(event){
     const input = document.getElementById('email')
     const div = document.querySelector('.email')
     const msgError = document.querySelector('.msg__error')
+    const iconError = document.querySelector('.icon__error')
 
     if(input.value === '') {
         msgError.style.display = 'block'
         div.classList.add('email__error')
+        iconError.style.display = 'block'
         event.preventDefault()
     }
 })
